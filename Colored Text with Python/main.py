fbf23ac220dc845016bbfb72c52b29a5a4b22c09
Avi Chaudhary <112181462+avichaudhary29@users.noreply.github.com>
2022-12-06 15:48:49 +0530

    Add files via upload

---
+++ Colored Text with Python/main.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hy!! My name is Avi Chaudhary"+ Fore.YELLOW+ Back.BLUE+"This is my Print Colored Text with Python Project")
+print(Back.LIGHTCYAN_EX+"Hy!! My name is Avi Chaudhary")
+print(Fore.RED + Back.LIGHTGREEN_EX+ "Hy!! My name is Avi Chaudhary")
